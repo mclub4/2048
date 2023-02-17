1ba2f0a321fd8f2fea35ba50734d1d24219f0c47
mclub4 <mclub901@gmail.com>
2023-02-18 03:02:54 +0900

    gameboard

--- function.js
+++ function.js
@@ -1,4 +1,28 @@
 
 function game_start(){
     alert("게임이 시작되었습니다!");
+    
+
+    // 게임판 생성
+    const game_board = document.createElement('table');
+
+    for(var i = 0; i<4; i++){
+        const tr = document.createElement('tr');
+        for(var j = 0; j<4; j++){
+            const td = document.createElement('td');
+            td.id = 4*i + j;
+            tr.appendChild(td);
+        }
+        game_board.appendChild(tr);
+    }
+
+    document.body.appendChild(game_board);
+}
+
+function generate(){
+
+}
+
+function move(){
+    
 }
