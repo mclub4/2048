be2a4c8f3b29e4f00b5d96221d6fd326b33bdec6
mclub4 <mclub901@gmail.com>
2023-02-18 20:16:13 +0900

    keyboardpress

--- function.js
+++ function.js
@@ -1,10 +1,21 @@
 
-function game_start(){
-    alert("게임이 시작되었습니다!");
-    
+const normal = 1;
+const challenge = 2; 
+const hard = 3;
+
+let weight;
+let score;
+let gameboard_array; //게임판 좌표의 데이터가 저장되는 2차원 배열
+const gameboard = document.createElement('table'); //시각적으로 보여지는 게임판
+
+function gameStart(){
+    // 난이도별 가중치 지정정
+    weight = parseInt(document.querySelector('input[name="mode"]:checked').value);
+    console.log(weight);
 
     // 게임판 생성
-    const game_board = document.createElement('table');
+    gameboard_array = Array.from(Array(4), () => new Array(4).fill(0))
+    console.table(gameboard_array);
 
     for(var i = 0; i<4; i++){
         const tr = document.createElement('tr');
@@ -13,16 +24,61 @@ function game_start(){
             td.id = 4*i + j;
             tr.appendChild(td);
         }
-        game_board.appendChild(tr);
+        gameboard.appendChild(tr);
     }
 
-    document.body.appendChild(game_board);
+    document.body.appendChild(gameboard);
+    score = 0;
+
+    randomXY();
 }
 
-function generate(){
+function randomXY(){
+    let rand = parseInt(Math.random()*16);
+    let y = parseInt(rand/4);
+    let x = rand % 4;
+    if(gameboard_array[x][y] == 0) gameboard_array[x][y] = randomNumber();
 
+    while(true){
+        rand = parseInt(Math.random()*16);
+        y = parseInt(rand/4);
+        x = rand % 4;
+        if(gameboard_array[x][y] == 0){
+            gameboard_array[x][y] = randomNumber();
+            return ;
+        }
+    }
 }
 
-function move(){
-    
+function randomNumber(){
+    let rand = parseInt(Math.random*10);
+    if(rand == 0 && weight == 3) return 8;
+    if(rand == 0) return 4;
+    else return 2;
+}
+
+// 키보드 입력 처리
+document.addEventListener('keydown', (e) => {
+    switch(e.key){
+        case 'ArrowLeft': console.log(e.key); break;
+        case 'ArrowRight': console.log(e.key); break;
+        case 'ArrowUp' : console.log(e.key); break;
+        case 'ArrowDown' : console.log(e.key); break;
+    }
+});
+
+function left(){
+    let isMoved ;    
+}
+
+function right(){
+    let isMoved ;    
+}
+
+function up(){
+    let isMoved ;    
+}
+
+function down(){
+    let isMoved ;    
 }
